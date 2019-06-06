5ebd010dfb58cdd5d017daeb63ed2e27432829cc
Rogier Spieker <rogier.spieker@magneds.com>
2019-06-06 09:03:14 +0200

    Updated dependencies, moved to the @hapi scoped packages for lab and code

--- test/Each.js
+++ test/Each.js
@@ -1,5 +1,5 @@
-const { expect } = require('code');
-const { after, before, describe, it } = exports.lab = require('lab').script();
+const { expect } = require('@hapi/code');
+const { describe, it } = exports.lab = require('@hapi/lab').script();
 
 const each = require('../source/tag.js');
 
