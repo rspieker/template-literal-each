bc23eddb9954ac875e84d9edf7759e5984f17d7c
Rogier Spieker <rogier@konfirm.io>
2022-08-24 14:18:41 +0200

    use longhand Array<> types

--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,13 @@
 import { table, create as tcreate, empty as tempty } from "template-literal-table";
 
-function iterator<T extends { [key: string]: unknown }, P extends Parameters<typeof table>>(parser: (...args: P) => T[]): (...args: P) => (callback: (record: T, index: number, all: T[]) => void) => void {
+function iterator<T extends { [key: string]: unknown }, P extends Parameters<typeof table>>(parser: (...args: P) => Array<T>): (...args: P) => (callback: (record: T, index: number, all: Array<T>) => void) => void {
 	return (...args: P) => {
-		const records: T[] = parser(...args);
+		const records: Array<T> = parser(...args);
 
-		return (callback: (record: T, index: number, all: T[]) => void) => records.forEach(callback);
+		return (callback: (record: T, index: number, all: Array<T>) => void) => records.forEach(callback);
 	};
 }
-export function create(...filters: ((...values: unknown[]) => boolean)[]): ReturnType<typeof iterator> {
+export function create(...filters: Array<((...values: Array<unknown>) => boolean)>): ReturnType<typeof iterator> {
 	return iterator(tcreate(...filters));
 }
 
